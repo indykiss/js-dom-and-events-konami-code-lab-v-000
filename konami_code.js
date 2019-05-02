0fb8320de804de0f8c40c66382351bc6c5841389
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-02 03:04:36 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
 
   let index = 0;
 
-  document.body.addEventListener('keydown',function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown',function(e) {
     const key = parseInt(e.detail || e.which);
     console.log(e.detail)
     console.log(e.which)
